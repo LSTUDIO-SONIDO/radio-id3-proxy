f59a7eee6c41559db3dc7759f0f10d68488de558
LSTUDIO-SONIDO <141097027+LSTUDIO-SONIDO@users.noreply.github.com>
2025-11-08 23:09:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,4 +26,7 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Servidor ID3 listo 🎧"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor ID3 listo 🎧 en puerto ${PORT}`));
+
+
